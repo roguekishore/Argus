f9330201af711cca816bf395c854b58ff61121bd
Kishore N E <contactforkishore@gmail.com>
2026-01-25 12:06:27 +0530

    feat: added shadcn components

---
+++ reactapp/src/components/ui/index.js
@@ -0,0 +1,11 @@
+// UI Components
+export { Button, buttonVariants } from "./button";
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card";
+export { Input } from "./input";
+export { Textarea } from "./textarea";
+export { Label } from "./label";
+export { Badge, badgeVariants } from "./badge";
+export { Skeleton } from "./skeleton";
+export { Separator } from "./separator";
+export { ScrollArea } from "./scroll-area";
+export { Avatar, AvatarImage, AvatarFallback } from "./avatar";
